bbc597b483afeab6708a48e4e737f4bea649cab6
sheeeeep3388 <75594739+sheeeeep3388@users.noreply.github.com>
2020-12-07 12:01:03 +0800

    20201207book
    上傳頁面飛翔

---
+++ 頁面飛翔/B3B56書本海鷗.js
@@ -0,0 +1,389 @@
+(function (cjs, an) {
+
+var p; // shortcut to reference prototypes
+var lib={};var ss={};var img={};
+lib.ssMetadata = [
+		{name:"B3B56書本海鷗_atlas_", frames: [[914,396,85,19],[924,310,94,23],[914,368,101,26],[890,492,107,29],[902,417,112,31],[826,586,123,32],[890,459,134,31],[862,555,143,29],[674,583,150,27],[834,620,157,23],[674,620,158,24],[852,523,162,30],[674,523,176,28],[674,555,186,26],[674,494,200,27],[674,459,214,33],[674,417,226,40],[674,368,238,47],[674,310,248,56],[674,244,258,64],[674,170,267,72],[674,89,276,79],[674,0,283,87],[0,0,672,670]]}
+];
+
+
+// symbols:
+
+
+
+(lib.CachedBmp_23 = function() {
+	this.initialize(ss["B3B56書本海鷗_atlas_"]);
+	this.gotoAndStop(0);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedBmp_22 = function() {
+	this.initialize(ss["B3B56書本海鷗_atlas_"]);
+	this.gotoAndStop(1);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedBmp_21 = function() {
+	this.initialize(ss["B3B56書本海鷗_atlas_"]);
+	this.gotoAndStop(2);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedBmp_20 = function() {
+	this.initialize(ss["B3B56書本海鷗_atlas_"]);
+	this.gotoAndStop(3);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedBmp_19 = function() {
+	this.initialize(ss["B3B56書本海鷗_atlas_"]);
+	this.gotoAndStop(4);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedBmp_18 = function() {
+	this.initialize(ss["B3B56書本海鷗_atlas_"]);
+	this.gotoAndStop(5);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedBmp_17 = function() {
+	this.initialize(ss["B3B56書本海鷗_atlas_"]);
+	this.gotoAndStop(6);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedBmp_16 = function() {
+	this.initialize(ss["B3B56書本海鷗_atlas_"]);
+	this.gotoAndStop(7);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedBmp_15 = function() {
+	this.initialize(ss["B3B56書本海鷗_atlas_"]);
+	this.gotoAndStop(8);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedBmp_14 = function() {
+	this.initialize(ss["B3B56書本海鷗_atlas_"]);
+	this.gotoAndStop(9);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedBmp_13 = function() {
+	this.initialize(ss["B3B56書本海鷗_atlas_"]);
+	this.gotoAndStop(10);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedBmp_12 = function() {
+	this.initialize(ss["B3B56書本海鷗_atlas_"]);
+	this.gotoAndStop(11);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedBmp_11 = function() {
+	this.initialize(ss["B3B56書本海鷗_atlas_"]);
+	this.gotoAndStop(12);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedBmp_10 = function() {
+	this.initialize(ss["B3B56書本海鷗_atlas_"]);
+	this.gotoAndStop(13);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedBmp_9 = function() {
+	this.initialize(ss["B3B56書本海鷗_atlas_"]);
+	this.gotoAndStop(14);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedBmp_8 = function() {
+	this.initialize(ss["B3B56書本海鷗_atlas_"]);
+	this.gotoAndStop(15);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedBmp_7 = function() {
+	this.initialize(ss["B3B56書本海鷗_atlas_"]);
+	this.gotoAndStop(16);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedBmp_6 = function() {
+	this.initialize(ss["B3B56書本海鷗_atlas_"]);
+	this.gotoAndStop(17);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedBmp_5 = function() {
+	this.initialize(ss["B3B56書本海鷗_atlas_"]);
+	this.gotoAndStop(18);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedBmp_4 = function() {
+	this.initialize(ss["B3B56書本海鷗_atlas_"]);
+	this.gotoAndStop(19);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedBmp_3 = function() {
+	this.initialize(ss["B3B56書本海鷗_atlas_"]);
+	this.gotoAndStop(20);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedBmp_2 = function() {
+	this.initialize(ss["B3B56書本海鷗_atlas_"]);
+	this.gotoAndStop(21);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedBmp_1 = function() {
+	this.initialize(ss["B3B56書本海鷗_atlas_"]);
+	this.gotoAndStop(22);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.修過 = function() {
+	this.initialize(ss["B3B56書本海鷗_atlas_"]);
+	this.gotoAndStop(23);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.頁面飛翔 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// 圖層_2
+	this.instance = new lib.CachedBmp_1();
+	this.instance.setTransform(50.45,178.6,0.5,0.5);
+
+	this.instance_1 = new lib.CachedBmp_2();
+	this.instance_1.setTransform(60.1,170.55,0.5,0.5);
+
+	this.instance_2 = new lib.CachedBmp_3();
+	this.instance_2.setTransform(70,162.75,0.5,0.5);
+
+	this.instance_3 = new lib.CachedBmp_4();
+	this.instance_3.setTransform(80.15,155.2,0.5,0.5);
+
+	this.instance_4 = new lib.CachedBmp_5();
+	this.instance_4.setTransform(90.5,147.8,0.5,0.5);
+
+	this.instance_5 = new lib.CachedBmp_6();
+	this.instance_5.setTransform(100.6,140.5,0.5,0.5);
+
+	this.instance_6 = new lib.CachedBmp_7();
+	this.instance_6.setTransform(111.25,132.8,0.5,0.5);
+
+	this.instance_7 = new lib.CachedBmp_8();
+	this.instance_7.setTransform(122,124.9,0.5,0.5);
+
+	this.instance_8 = new lib.CachedBmp_9();
+	this.instance_8.setTransform(132.9,116.95,0.5,0.5);
+
+	this.instance_9 = new lib.CachedBmp_10();
+	this.instance_9.setTransform(143.9,108.9,0.5,0.5);
+
+	this.instance_10 = new lib.CachedBmp_11();
+	this.instance_10.setTransform(155,100.85,0.5,0.5);
+
+	this.instance_11 = new lib.CachedBmp_12();
+	this.instance_11.setTransform(166.15,92.8,0.5,0.5);
+
+	this.instance_12 = new lib.CachedBmp_13();
+	this.instance_12.setTransform(149.75,85,0.5,0.5);
+
+	this.instance_13 = new lib.CachedBmp_14();
+	this.instance_13.setTransform(133.95,77,0.5,0.5);
+
+	this.instance_14 = new lib.CachedBmp_15();
+	this.instance_14.setTransform(119.05,68.7,0.5,0.5);
+
+	this.instance_15 = new lib.CachedBmp_16();
+	this.instance_15.setTransform(103.95,60.2,0.5,0.5);
+
+	this.instance_16 = new lib.CachedBmp_17();
+	this.instance_16.setTransform(89.4,51.35,0.5,0.5);
+
+	this.instance_17 = new lib.CachedBmp_18();
+	this.instance_17.setTransform(75.15,42.7,0.5,0.5);
+
+	this.instance_18 = new lib.CachedBmp_19();
+	this.instance_18.setTransform(60.9,35.25,0.5,0.5);
+
+	this.instance_19 = new lib.CachedBmp_20();
+	this.instance_19.setTransform(45.25,25.8,0.5,0.5);
+
+	this.instance_20 = new lib.CachedBmp_21();
+	this.instance_20.setTransform(29.9,17.3,0.5,0.5);
+
+	this.instance_21 = new lib.CachedBmp_22();
+	this.instance_21.setTransform(14.85,8.75,0.5,0.5);
+
+	this.instance_22 = new lib.CachedBmp_23();
+	this.instance_22.setTransform(0,0,0.5,0.5);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance}]}).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_2}]},1).to({state:[{t:this.instance_3}]},1).to({state:[{t:this.instance_4}]},1).to({state:[{t:this.instance_5}]},1).to({state:[{t:this.instance_6}]},1).to({state:[{t:this.instance_7}]},1).to({state:[{t:this.instance_8}]},1).to({state:[{t:this.instance_9}]},1).to({state:[{t:this.instance_10}]},1).to({state:[{t:this.instance_11}]},1).to({state:[{t:this.instance_12}]},1).to({state:[{t:this.instance_13}]},1).to({state:[{t:this.instance_14}]},1).to({state:[{t:this.instance_15}]},1).to({state:[{t:this.instance_16}]},1).to({state:[{t:this.instance_17}]},1).to({state:[{t:this.instance_18}]},1).to({state:[{t:this.instance_19}]},1).to({state:[{t:this.instance_20}]},1).to({state:[{t:this.instance_21}]},1).to({state:[{t:this.instance_22}]},1).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(0,0,247.2,222.1);
+
+
+// stage content:
+(lib.B3B56書本海鷗 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// 圖層_2
+	this.instance = new lib.頁面飛翔();
+	this.instance.setTransform(173,258.95,1,1,0,0,0,121.2,200.2);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
+
+	// 原稿
+	this.instance_1 = new lib.修過();
+	this.instance_1.setTransform(0,0,0.5,0.5);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance_1).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(168,167.5,168,167.5);
+// library properties:
+lib.properties = {
+	id: '2D668A907084354ABFE8AC6BD1D232DF',
+	width: 336,
+	height: 335,
+	fps: 10,
+	color: "#FF99CC",
+	opacity: 1.00,
+	manifest: [
+		{src:"images/B3B56書本海鷗_atlas_.png", id:"B3B56書本海鷗_atlas_"}
+	],
+	preloads: []
+};
+
+
+
+// bootstrap callback support:
+
+(lib.Stage = function(canvas) {
+	createjs.Stage.call(this, canvas);
+}).prototype = p = new createjs.StageGL();
+
+p.setAutoPlay = function(autoPlay) {
+	this.tickEnabled = autoPlay;
+}
+p.play = function() { this.tickEnabled = true; this.getChildAt(0).gotoAndPlay(this.getTimelinePosition()) }
+p.stop = function(ms) { if(ms) this.seek(ms); this.tickEnabled = false; }
+p.seek = function(ms) { this.tickEnabled = true; this.getChildAt(0).gotoAndStop(lib.properties.fps * ms / 1000); }
+p.getDuration = function() { return this.getChildAt(0).totalFrames / lib.properties.fps * 1000; }
+
+p.getTimelinePosition = function() { return this.getChildAt(0).currentFrame / lib.properties.fps * 1000; }
+
+an.bootcompsLoaded = an.bootcompsLoaded || [];
+if(!an.bootstrapListeners) {
+	an.bootstrapListeners=[];
+}
+
+an.bootstrapCallback=function(fnCallback) {
+	an.bootstrapListeners.push(fnCallback);
+	if(an.bootcompsLoaded.length > 0) {
+		for(var i=0; i<an.bootcompsLoaded.length; ++i) {
+			fnCallback(an.bootcompsLoaded[i]);
+		}
+	}
+};
+
+an.compositions = an.compositions || {};
+an.compositions['2D668A907084354ABFE8AC6BD1D232DF'] = {
+	getStage: function() { return exportRoot.stage; },
+	getLibrary: function() { return lib; },
+	getSpriteSheet: function() { return ss; },
+	getImages: function() { return img; }
+};
+
+an.compositionLoaded = function(id) {
+	an.bootcompsLoaded.push(id);
+	for(var j=0; j<an.bootstrapListeners.length; j++) {
+		an.bootstrapListeners[j](id);
+	}
+}
+
+an.getComposition = function(id) {
+	return an.compositions[id];
+}
+
+
+an.makeResponsive = function(isResp, respDim, isScale, scaleType, domContainers) {		
+	var lastW, lastH, lastS=1;		
+	window.addEventListener('resize', resizeCanvas);		
+	resizeCanvas();		
+	function resizeCanvas() {			
+		var w = lib.properties.width, h = lib.properties.height;			
+		var iw = window.innerWidth, ih=window.innerHeight;			
+		var pRatio = window.devicePixelRatio || 1, xRatio=iw/w, yRatio=ih/h, sRatio=1;			
+		if(isResp) {                
+			if((respDim=='width'&&lastW==iw) || (respDim=='height'&&lastH==ih)) {                    
+				sRatio = lastS;                
+			}				
+			else if(!isScale) {					
+				if(iw<w || ih<h)						
+					sRatio = Math.min(xRatio, yRatio);				
+			}				
+			else if(scaleType==1) {					
+				sRatio = Math.min(xRatio, yRatio);				
+			}				
+			else if(scaleType==2) {					
+				sRatio = Math.max(xRatio, yRatio);				
+			}			
+		}			
+		domContainers[0].width = w * pRatio * sRatio;			
+		domContainers[0].height = h * pRatio * sRatio;			
+		domContainers.forEach(function(container) {				
+			container.style.width = w * sRatio + 'px';				
+			container.style.height = h * sRatio + 'px';			
+		});			
+		stage.scaleX = pRatio*sRatio;			
+		stage.scaleY = pRatio*sRatio;			
+		lastW = iw; lastH = ih; lastS = sRatio;            
+		stage.tickOnUpdate = false;            
+		stage.update();            
+		stage.tickOnUpdate = true;		
+	}
+}
+
+
+})(createjs = createjs||{}, AdobeAn = AdobeAn||{});
+var createjs, AdobeAn;
